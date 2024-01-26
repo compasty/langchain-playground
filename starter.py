80cc236d2d7892596e8a8bcdd29c3819c4f0b725
yanhuai.yh <yanhuai.yh@antgroup.com>
2024-01-26 17:24:02 +0800

    feat: Initial commit

---
+++ starter.py
@@ -0,0 +1,15 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+output_parser = StrOutputParser()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are world class technical documentation writer."),
+    ("user", "{input}")
+])
+
+llm = ChatOpenAI()
+llm.invoke("how can langsmith help with testing?")
+
+chain = prompt | llm | output_parser
